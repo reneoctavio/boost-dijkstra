5d283054de7c5ce63ab0f3d6215f280936e0b3c6
Rene Octavio Queiroz Dias <reneoctavio@gmail.com>
2015-12-05 22:15:37 -0200

    Initial Commit

---
+++ boost_dijkstra/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  boost_dijkstra
+//
+//  Created by Rene Octavio Queiroz Dias on 12/5/15.
+//  Copyright © 2015 Rene Octavio Queiroz Dias. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
